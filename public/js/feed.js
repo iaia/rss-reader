87c55620e7f535792eb20f559ce72835ccea6935
iaia <iaia72160@gmail.com>
2016-02-02 22:40:23 +0900

    articleの下にarticle_lineが来るように それに伴ってjsが動かなかったので修正

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,24 +1,30 @@
 var shiftkey;
 var nowArticle;
 var maxArticle;
-$(function(){
+var previousArticle;
+$(document).ready( function(){
     shiftkey = false;
     nowArticle = 0;
     maxArticle = 25;
+    previousArticle = 0;
 });
 
 $(function(){
     $(".article_line").click(function(){
-        previousArticle = nowArticle;
+        if(previousArticle != 0){
+            article = document.getElementById("article_" + String(previousArticle));
+            contentToggle(article);
+        }
+ 
         nowArticle = Number(this.id.split("article_")[1]);
-
+        previousArticle = nowArticle;
         article = document.getElementById("article_" + String(nowArticle));
+
         contentToggle(article);
     });
 });
 
 $(window).keydown(function(e){
-    console.log("ok");
     var keycode = e.keyCode;
     if(keycode == 16){ // shift
         shiftkey = true;
@@ -31,11 +37,12 @@ $(window).keydown(function(e){
 });
 
 function selectedArticle(down){
+    previousArticle = nowArticle;
     if(down == true){ // 下
         if(nowArticle >= maxArticle){
             return;
         }
-        article = document.getElementById("article_" + String(nowArticle));
+        article = document.getElementById("article_" + String(previousArticle));
         contentToggle(article); // 前のを閉じる
         nowArticle += 1;
         article = document.getElementById("article_" + String(nowArticle));
@@ -46,7 +53,7 @@ function selectedArticle(down){
             if(nowArticle <= 1){
                 return;
             }
-            article = document.getElementById("article_" + String(nowArticle));
+            article = document.getElementById("article_" + String(previousArticle));
             contentToggle(article); // 前のを閉じる
             nowArticle -= 1;
             article = document.getElementById("article_" + String(nowArticle));
@@ -56,7 +63,7 @@ function selectedArticle(down){
 }
 
 function contentToggle(article){
-    $(article).children(".article_line_content").toggle();
-    $(article).children(".article_content").toggle();
-    $(article).get(0).scrollIntoView(true);
+    $(article).children(".article_line_title_and_content").children(".article_line_content").toggle();
+    $(article).parent().children(".article_content").toggle();
+    $(article).scrollTop($(article).position())
 }
