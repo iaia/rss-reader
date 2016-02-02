4ef127ac78e7dc586f1f3df4b286c730e5031b33
iaia <iaia72160@gmail.com>
2016-02-03 01:06:55 +0900

    add部分を別ページにしたのでそのページを作った

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,28 +1,16 @@
 class FeedController < ApplicationController
     include Feed
-    before_action :auth
+    before_action :auth, :update
 
     def index
-        @sites = @user.sites
-        @sites.each do |site|
-            Article.update_site_articles(site)
-        end
-        @articles = Article.where(read: false)
     end
 
     def new
-        @sites = @user.sites
-        @sites.each do |site|
-            Article.update_site_articles(site)
-        end
-        @articles = Article.where(read: false)
- 
     end
 
     def preview
         @url = params[:add_feed][:url]
-        feed = Feed::GetFeed.new()
-        @site = feed.get_feed(@url)
+        @site = Feed.get_feed(@url)
         @articles = Article.preview(@url)
     end
 
@@ -34,7 +22,6 @@ class FeedController < ApplicationController
     def site
         @sites = @user.sites
         @site = @user.sites.find(params[:id])
-        @articles = Article.where(read: false)
 
         render action: "index"
     end
@@ -42,4 +29,13 @@ class FeedController < ApplicationController
     def auth
         @user = User.find(1)
     end
+
+    def update
+        @sites = @user.sites
+        @sites.each do |site|
+            #Article.update_site_articles(site)
+        end
+        @articles = Article.where(read: false)
+    end
+ 
 end
