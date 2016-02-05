5648e5d22f11e7998d967202b2911292e12981a0
iaia <iaia72160@gmail.com>
2016-02-06 08:48:26 +0900

    linkが取れてなかったのを修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,108 +5,33 @@ class Article < ActiveRecord::Base
         if feed_url.blank?
             return 
         end
-        rss = Feed.get_feed(feed_url)
-        articles = []
-        rss_class = rss.class.to_s 
-        p rss_class
-        if rss_class == "RSS::Atom::Feed"
-            rss_version = ""
-        else
-            rss_version = rss.rss_version
-        end
-        if rss_class == "RSS::Rss" and rss_version == "2.0"
-            rss.items.each do |entry|
-                article = Article.create(
-                    title: entry.title.to_s,
-                    #entry_id: entry.id.content,
-                    url: entry.link.to_s,
-                    #feed_url: entry.links.to_s,
-                    published: entry.pubDate.to_s,
-                    content: entry.content_encoded.to_s
-                )
-                articles << article
-            end
-        elsif rss_class == "RSS::Atom::Feed"
-            rss.entries.each do |entry|
-                p entry.title.content
-                p entry.id.content
-                p entry.links[4].href
-                p entry.links[2].href
-                p entry.published.content
-                p entry.content.content
-
-                article = Article.create(
-                    title: entry.title.content,
-                    entry_id: entry.id.content,
-                    url: entry.links[4].href,
-                    feed_url: entry.links[2].href,
-                    published: entry.published.content,
-                    content: entry.content.content
-                )
-                articles << article
-            end
-        elsif rss_class == "RSS::RDF" and rss_version == "1.0"
-            rss.channel.each do |entry|
-                article = site.articles.build(
-                    title: entry.title.to_s,
-                    #entry_id: entry.id.content,
-                    url: entry.link.to_s,
-                    #feed_url: entry.[2].href,
-                    published: entry.dc_dates[0].content.to_s,
-                    content: entry.description.to_s
-                )
-                articles << article
-            end
+        rss = Feed.get(feed_url)
+        site_info, entries = Feed.read(rss)
+        p entries
+        entries.each do |entry|
+            article = Article.create(
+                title: entry["title"],
+                url: entry["url"],
+                published: entry["published_time"],
+                description: entry["description"],
+                content: entry["content"]
+            )
+            articles << article
         end
         return articles
     end
     def self.update_site_articles(site)
-        rss = Feed.get_feed(site.feed_url)
-        if rss.class.to_s == "RSS::Rss" and rss.rss_version == "2.0"
-            rss.channel.each do |entry|
-                if site.articles.exists?(entry_id: entry.id.content)
-                    next
-                end
-                article = site.articles.build(
-                    title: entry.title.to_s,
-                    #entry_id: entry.id.content,
-                    url: entry.links.to_s,
-                    #feed_url: entry.links.to_s,
-                    published: entry.pubDate.to_s,
-                    content: entry.content_encoded.content
-                )
-                article.save
-            end
-        elsif rss.class.to_s == "RSS::Atom::Feed"
-            rss.entries.each do |entry|
-                if site.articles.exists?(entry_id: entry.id.content)
-                    next
-                end
-                article = site.articles.build(
-                    title: entry.title.content,
-                    entry_id: entry.id.content,
-                    url: entry.links[4].href,
-                    feed_url: entry.links[2].href,
-                    published: entry.published.content,
-                    content: entry.content.content
-                )
-                article.save
-            end
-        elsif rss.class.to_s == "RSS::RDF" and rss.rss_version == "1.0"
-            rss.channel.each do |entry|
-                if site.articles.exists?(entry_id: entry.id.content)
-                    next
-                end
-                article = site.articles.build(
-                    title: entry.title.to_s,
-                    #entry_id: entry.id.content,
-                    url: entry.link.to_s,
-                    #feed_url: entry.[2].href,
-                    published: entry.dc_dates[0].content.to_s,
-                    content: entry.description.to_s
-                )
-                article.save
-            end
+        rss = Feed.get(feed_url)
+        site_info, entries = Feed.read(rss)
+        entries.each do |entry|
+            article = Article.create(
+                title: entry["title"],
+                url: entry["url"],
+                published: entry["published_time"],
+                description: entry["description"],
+                content: entry["content"]
+            )
+            article.save
         end
     end
 end
--- lib/feed.rb
+++ lib/feed.rb
@@ -4,20 +4,74 @@ require 'rss'
 
 module Feed
     class << self
-        def get_feed(url)
+        def get(url)
             return RSS::Parser.parse(url, false)
         end
 
-        def show_feed(rss)
+        def read(rss)
+            site = Hash.new
+            entries = Array.new
+            site["class"] = rss.class.to_s
+            if rss.class.to_s == "RSS::Rss" and rss.rss_version == "2.0"
+                channel = rss.channel
+                site["version"] = rss.rss_version
+                site["title"] = channel.title.to_s
+                site["site_url"] = channel.link.to_s
+                channel.items.each do |item|
+                    entry = Hash.new
+                    entry["title"] = item.title.to_s
+                    entry["url"] = item.link.to_s
+                    entry["published_time"] = item.pubDate.to_s
+                    entry["description"] = item.content_encoded.to_s
+                    entry["content"] = item.content_encoded.to_s
+                    entries.push(entry)
+                end
+            elsif rss.class.to_s == "RSS::RDF" and rss.rss_version == "1.0"
+                channel = rss.channel
+                site["version"] = rss.rss_version
+                site["title"] = channel.title.to_s
+                site["site_url"] = channel.link.to_s
+                channel.items.each do |item|
+                    entry = Hash.new
+                    entry["title"] = item.title.to_s
+                    entry["url"] = item.link.to_s
+                    entry["published_time"] = item.dc_dates[-1].content.to_s
+                    entry["content"] = item.content_encoded.to_s
+                    entry["description"] = item.description.to_s
+                    entries.push(entry)
+                end
+            elsif rss.class.to_s == "RSS::Atom::Feed"
+                site["version"] = ""
+                site["title"] = rss.title.content
+                site["site_url"] = rss.links[2].href
+                rss.entries.each do |_entry|
+                    entry = Hash.new
+                    entry["title"] = _entry.title.content
+                    entry["url"] = _entry.links[-1].href
+                    if _entry.published.nil?
+                        entry["published_time"] = Time.now
+                    else
+                        entry["published_time"] = _entry.published.content
+                    end
+                    entry["content"] = _entry.content.content
+                    entry["description"] = _entry.content.content
+                    entries.push(entry)
+                end
+            else
+                site["version"] = ""
+                site["title"] = rss.title.content
+                site["site_url"] = rss.links[2].href
+            end
+            return site, entries
+        end
+
+        def show(rss)
             if rss.class.to_s == "RSS::Rss" and rss.rss_version == "2.0"
                 channel = rss.channel
                 p channel.title.to_s
-                # feed_url
                 p channel.link.to_s
                 channel.items.each do |item|
                     p item.title.to_s
-                    # feed_id
-                    # feed_url
                     p item.link.to_s
                     p item.pubDate.to_s
                     p item.content_encoded.to_s
@@ -29,10 +83,9 @@ module Feed
                 p channel.link.to_s
                 rss.items.each do |item|
                     p item.title.to_s
-                    # feed_id
-                    # feed_url
                     p item.link.to_s
-                    p item.dc_dates[0].content.to_s
+                    p item.dc_dates[-1].content.to_s
+                    p item.content_encoded.to_s
                     p item.description.to_s
                 end
             elsif rss.class.to_s == "RSS::Atom::Feed"
@@ -41,20 +94,23 @@ module Feed
                 p rss.links[2].href
                 rss.entries.each do |entry|
                     p entry.title.content
-                    p entry.id.content
-                    p entry.links[4].href
-                    p entry.links[2].href
-                    p entry.published.content
+                    p entry.links[-1].href
                     p entry.content.content
+                    if entry.published.nil?
+                    else
+                        p entry.published.content
+                    end
                 end
+            else
+                p rss.class.to_s 
             end
         end
     end
 end
 
-#url = "http://iaiaie.blogspot.com/feeds/posts/default" # atom
-#url = "http://blog.livedoor.jp/nicovip2ch/atom.xml" # 壊れたatom
-#url = "http://ie.u-ryukyu.ac.jp/news-ie/feed/" # rss2.0
+#url = "http://iaiaie.blogspot.com/feeds/posts/default" # atom atom11
+#url = "http://blog.livedoor.jp/nicovip2ch/atom.xml" # 壊れたatom atom10
+#url = "http://ie.u-ryukyu.ac.jp/news-ie/feed/" # rss2.0 rss20
 #url = "http://feeds.feedburner.com/mactegaki?format=xml" # RDF1.0
-#rss = Feed.get_feed(url)
-#feed.show_feed(rss)
+#rss = Feed.get(url)
+#Feed.read(rss)
