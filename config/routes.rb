69fd6393d4da5eb42ba2e6f21ba2a9e6b2400f61
iaia <iaia72160@gmail.com>
2019-02-11 20:23:45 +0900

    bin/rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
-
-  root to: 'feed#index'
-  get 'feed/show' => 'feed#show'
-  post 'feed/preview' => 'feed#preview'
-  post 'feed/add' => 'feed#add'
-
-  get 'feed/site' => 'feed#site'
-  get 'feed/site/:id' => 'feed#site'
-  get 'feed/new' => 'feed#new'
-
-  get 'setting/setting' => 'settings#setting'
-  get 'setting/import_opml' => 'settings#import_opml'
-  post 'setting/preview_opml' => 'settings#preview_opml'
-  post 'setting/regist_opml' => 'settings#regist_opml'
-  get 'setting/regist_opml' => 'settings#regist_opml'
-
-  get 'users/login'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
